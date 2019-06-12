32f112dacd1ea06770211247ee51337bd2c16055
Cheolho kim <gghg0000@gmail.com>
2019-06-12 20:00:14 +0900

    2019.06.12 19:59 kch

---
+++ pro26/src/main/test/cheolho/Test.java
@@ -0,0 +1,15 @@
+package cheolho;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        Object x = "buy";
+        String i = "Effective Java!";
+
+        x += i;
+        System.out.println(x);
+        Date
+    }
+}
